17fcf8f70ede89feb90deeed0859c1e11c71e3bc
695509060@qq.com <695509060@qq.com>
2020-10-07 22:43:47 +0800

    增加ner模块

---
+++ roberta/data/ner_dataset.py
@@ -0,0 +1,14 @@
+import glob
+import math
+import pkuseg
+import numpy as np
+
+from tqdm import tqdm
+from roberta.common.tokenizers import Tokenizer
+from pretrain_config import *
+from torch.utils.data import Dataset
+
+
+class NerDataSet(Dataset):
+    def __init__(self, corpus_path):
+        pass
