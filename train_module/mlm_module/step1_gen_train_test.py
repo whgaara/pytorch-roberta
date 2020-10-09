bb7d23b5e24e7af7f7cc225ccdad2d74b2580ebe
whgaara <695509060@qq.com>
2020-10-09 10:50:14 +0800

    up

--- train_module/mlm_module/step1_gen_train_test.py
+++ train_module/mlm_module/step1_gen_train_test.py
@@ -57,18 +57,19 @@ def gen_train_test():
     f_train = open('../../data/train_data/train.txt', 'w', encoding='utf-8')
     f_test = open('../../data/test_data/test.txt', 'w', encoding='utf-8')
 
-    filenames = glob.glob('%s/*.txt' % SourcePath)
-    np.random.shuffle(filenames)
-    for filename in filenames:
-        with open(filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                line = line.strip()
-                rad = random.randint(0, 10)
-                if rad < 1:
-                    f_test.write(line + '-***-' + random_wrong(line) + '\n')
-                    f_train.write(line + '\n')
-                else:
-                    f_train.write(line + '\n')
+    # filenames = glob.glob('%s/*.txt' % SourcePath)
+    # np.random.shuffle(filenames)
+    # for filename in filenames:
+    filename = '../../data/src_data/src_data.txt'
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            rad = random.randint(0, 10)
+            if rad < 5:
+                f_test.write(line + '-***-' + random_wrong(line) + '\n')
+                f_train.write(line + '\n')
+            else:
+                f_train.write(line + '\n')
 
     f_train.close()
     f_test.close()
