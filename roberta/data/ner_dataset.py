859e433fb287adf449586a68f52d1aa9239ce66c
695509060@qq.com <695509060@qq.com>
2020-10-15 17:59:19 +0800

    新增ner解析功能

--- roberta/data/ner_dataset.py
+++ roberta/data/ner_dataset.py
@@ -57,31 +57,56 @@ class NerDataSet(Dataset):
 
     def parse_ori_line(self, ori_line):
         """
-        :param ori_line: 六味地黄{ypcf,3}丸{yplb,1}
+        :param ori_line: 六味地黄{3,ypcf}丸{1,yplb}
         :return:
         [123, 233, 334, 221, 299, ...]
         [b-ypcf, i-ypcf, i-ypcf, e-ypcf, e-yplb]
         """
+        ori_line = ori_line.strip().replace(' ', '')
+        input_tokens = ''
         input_token_ids = []
         input_token_classes = []
+        i = 0
+        ori_line_list = list(ori_line)
+        while i < len(ori_line_list):
+            i += 1
+            if ori_line_list[i] != '{' and ori_line_list[i] != '}':
+                input_tokens += ori_line_list[i]
+                input_token_classes.append(0)
+            if ori_line_list[i] == '{':
+                current_type = ''
+                current_len = ''
+                j = i
+                while True:
+                    j += 1
+                    if ori_line_list[j].isdigit():
+                        current_len += ori_line_list[j]
+                    if ori_line_list[j] == ',':
+                        break
+                while True:
+                    j += 1
+                    current_type += ori_line_list[j]
+                    if ori_line_list[j] == '}':
+                        break
 
-
+                current_len = int(current_len)
+                if current_len == 1:
+                    input_token_classes[i - 1] = 'e' + current_type
+                elif current_len == 2:
+                    input_token_classes[i - 2] = 'b' + current_type
+                    input_token_classes[i - 1] = 'e' + current_type
+                else:
+                    input_token_classes[i - current_len] = 'b' + current_type
+                    input_token_classes[i - 1] = 'e' + current_type
+                    for k in range(current_len - 2):
+                        input_token_classes[i - 2 - k] = 'i' + current_type
+                i = j
 
         return input_token_ids, input_token_classes
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    dataset = NerDataSet(NerSourcePath)
+    for i, data in dataset:
+        print(i)
+        xxx = data
